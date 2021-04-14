a7fe01f0e0f0c14d859a286c7d037aaf02b0dced
Walidnurudin <walidnurudin47@gmail.com>
2021-04-14 09:41:16 +0700

    redux & router

---
+++ redux.js
@@ -0,0 +1,40 @@
+const redux = require('redux')
+const createStore = redux.createStore;
+
+// STATE
+const initialeState = {
+    value: 0,
+    age: 20,
+}
+
+// REDUCER
+const rootReducer = (state = initialeState, action) => {
+    switch(action.type){
+        case 'ADD_AGE':
+        return {
+            ...state,
+            age: state.age++,
+        }
+        case 'CHANGE_VALUE':
+        return {
+            ...state,
+            value: state.value + action.newValue,
+        }
+        default:
+            return state;
+    }
+}
+
+// STORE
+const store = createStore(rootReducer);
+console.log(store.getState())
+
+// SUBCRIPTION
+store.subscribe(() => {
+    console.log('store change', store.getState());
+})
+
+// DISPATCHING ACTION
+store.dispatch({type: 'ADD_AGE'});
+store.dispatch({type: 'CHANGE_VALUE', newValue: 12});
+console.log(store.getState());
